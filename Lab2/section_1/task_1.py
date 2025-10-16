558fb8df8077572072a51997b092b2529cef1990
timoteo-tr <usuario_cocyten_55_x@cinvestav.mx>
2025-10-15 19:55:25 -0700

    Primera versión de conversión de tipos de datos

---
+++ Lab2/section_1/task_1.py
@@ -0,0 +1,15 @@
+var1 = "123"
+print(var1)
+print(3*var1)
+print()
+var2 = int(var1)
+print(var2)
+print(3*var2, "\n")
+      
+var3=float(var1)
+print(var3)
+print(3*var3)
+
+var4 = bool(var1)
+print(var4)
+print(3*var4)
